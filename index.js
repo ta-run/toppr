e2f39e5591e798095f69c8f6619256d88db3abe0
Tarun Pai <Tarun Pai>
2016-09-17 22:02:24 +0530

    removed unnecessary code

--- index.js
+++ index.js
@@ -24,62 +24,6 @@ var init = {
                 }
             }
         })
-
-        Server.route({
-            method: 'GET',
-            path: '/websites',
-            handler: (request, reply) => {
-                reply([{
-                    title: 'Daniel g. Siegel',
-                    favicon_image: 'asd.png',
-                    url_address: 'dgsiegel.net',
-                    tag: 'Personal',
-                    like_counter: 2
-                }, {
-                    title: 'Ross Pernman',
-                    favicon_image: 'asd.png',
-                    url_address: 'rosspenman.com',
-                    tag: 'Personal',
-                    like_counter: 2
-                }, {
-                    title: 'goker/resume',
-                    favicon_image: 'asd.png',
-                    url_address: 'gokercebeci.com/me',
-                    tag: 'Blog',
-                    like_counter: 2
-                }, {
-                    title: 'Gilles Quenot / SO',
-                    favicon_image: 'asd.png',
-                    url_address: 'goo.gl/fdr5Kq',
-                    tag: 'Social',
-                    like_counter: 2
-                }, {
-                    title: 'Nithin Rao Kumlekar',
-                    favicon_image: 'asd.png',
-                    url_address: 'nithinkumblekar.com',
-                    tag: 'Caricature',
-                    like_counter: 2
-                }, {
-                    title: 'webqwe',
-                    favicon_image: 'qwe.png',
-                    url_address: 'qwe.com',
-                    tag: 'Personal',
-                    like_counter: 2
-                }, {
-                    title: 'webasd',
-                    favicon_image: 'asd.png',
-                    url_address: 'asd.com',
-                    tag: 'Personal',
-                    like_counter: 2
-                }, {
-                    title: 'webzxc',
-                    favicon_image: 'zxc.png',
-                    url_address: 'zxc.com',
-                    tag: 'Personal',
-                    like_counter: 2
-                }])
-            }
-        })
     },
     server: () => {
         Server.start(() => { console.log(`Listening on port ${Server.info.uri}`) })
